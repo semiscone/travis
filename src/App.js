5653f611c7234d3872319c62c646e3b38957d9bb
William Yang <weiyan2@cisco.com>
2018-11-22 13:35:14 +0800

    Updated it based on StephenGrider/docker-react.git

--- src/App.js
+++ src/App.js
@@ -8,18 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hi there!
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <p className="App-intro">
+          I was changed on the Feature branch
+        </p>
       </div>
     );
   }
